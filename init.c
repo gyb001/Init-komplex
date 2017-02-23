1b63492b5a4e1ca07d28803f2dc849c524034ba6
root <root@debian>
2017-02-23 19:36:15 +0100

    ssh szerver működik

--- init.c
+++ init.c
@@ -1,24 +1,29 @@
 #define _XOPEN_SOURCE 700
 #include<signal.h>
 #include <unistd.h>
-#include<stdio.h>
-
+#include<stdio.h> //printf
+#include<stdlib.h> //system hívás miatt
 int main()
 {
 
 	sigset_t set;
 	int status;
 
-	if(getpid() != 1 ) return 1;
-	
 	sigfillset(&set);
 	sigprocmask(SIG_BLOCK, &set, 0);
 
 	if (fork()) for(;;) wait(&status);
 
 	sigprocmask(SIG_UNBLOCK, &set, 0);
-	printf("Mivan INIT :DD");
+
 	setsid();
 	setpgid(0,0);
+
+
+	int  mountkernStatus=  system("./etc/init.d/mountkernfs.sh start");
+	int  mountStatus    =  system("./etc/init.d/mountall.sh");
+	int  networkStatus  =  system("./etc/init.d/networking start");
+	int  sshStatus      =  system("./etc/init.d/ssh start");
+
 	return execve("/etc/rc", (char *[]){ "rc",0 }, (char *[]){0});
 }
